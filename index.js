682b5b18a250c6b0b6f8a32f28f61d11ef2597a4
Margarita Morozova <ritamorozova@yahoo.com>
2021-01-19 11:51:07 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
+  let min = array[0]
+  let indexOfmin = 0
 
+  for(let i=0; i< array.length; i++){
+      if(min > array[i]){
+          min = array[i]
+          indexOfmin = i
+      }
+  }
+  array.splice(indexOfmin, 1)
+  return min
 }
 
 function selectionSort(array){
-
+  let newMin;
+  let sorted = []
+  while(array.length !=0){
+      newMin = findMinAndRemove(array)
+      sorted.push(newMin)
+  }
+  return sorted 
 }
